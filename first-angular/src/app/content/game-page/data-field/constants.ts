019ecb79a4a3cd2fde679a5558d363b13dd9ceb3
Nikita Volkov <n.volkov1@g.nsu.ru>
2023-10-25 23:38:10 +0700

    Turn constant and map-field

---
+++ first-angular/src/app/content/game-page/data-field/constants.ts
@@ -0,0 +1,4 @@
+export enum Turn {
+  MY,
+  OPPONENT,
+}
--- first-angular/src/app/services/game-control.service.ts
+++ first-angular/src/app/services/game-control.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { GamePhases, GameState } from '../constants';
 import { Fractions } from '../content/game-page/preparing/constants';
+import { Turn } from '../content/game-page/data-field/constants';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,8 @@ export class GameControlService {
   selectedFraction: Fractions | null = null;
 
   gameState: GameState = 0;
+
+  turn: Turn = 0;
 
   handleFractionChoice(fractionName: Fractions) {
     this.selectedFraction = fractionName;
@@ -26,5 +29,13 @@ export class GameControlService {
 
   handleGameEnded() {
     this.gameState = 0;
+  }
+
+  handleTurn() {
+    if (this.turn === 0) {
+      this.turn = 1;
+    } else {
+      this.turn = 0;
+    }
   }
 }
